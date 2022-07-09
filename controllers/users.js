f1a250e9893f1584c52dd15d1e740157039e65d7
lauk20 <klau20@stuy.edu>
2022-07-09 13:30:16 -0400

    Add users controller to create users

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,13 @@
 const router = require("mongoose").Router()
+const bcrypt = require("bcrypt")
 const User = require("../models/user")
 
 router.post("/", async (request, response) => {
   const { username, password } = request.body
+  const takenUser = await User.findOne({username: username})
+  if (takenUser) {
+    return response.status(400).json({error: "username already taken"})
+  }
 
   const saltrounds = 12
   const hash = await bcrypt.hash(password, saltrounds);
