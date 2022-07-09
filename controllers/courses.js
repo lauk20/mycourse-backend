4ae21b2aa016a0e406e558a0f2724c00c2cfda06
lauk20 <klau20@stuy.edu>
2022-07-09 13:33:32 -0400

    Update courses to only allow authorized users

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,13 +1,35 @@
 const router = require("express").Router()
 const Course = require("../models/course")
 
+const getToken = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")){
+    return authorization.substring(7)
+  }
+
+  return null;
+}
+
 router.get("/", (request, response) => {
-  Course.find({}).populate("assignments").then((courses) => {response.json(courses)})
+  const token = getToken(request);
+  const decodedToken = jwt.verify(token, process.env.JWTTOKEN);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "invalid token" })
+  }
+
+  Course.find({user: decodedToken.id}).populate("assignments").then((courses) => {response.json(courses)})
 })
 
 router.post("/", async (request, response) => {
+  const token = getToken(request);
+  const decodedToken = jwt.verify(token, process.env.JWTTOKEN);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "invalid token" })
+  }
+
   const course = new Course({
     name: request.body.name,
+    user: decodedToken.id,
   })
 
   const savedCourse = await course.save()
