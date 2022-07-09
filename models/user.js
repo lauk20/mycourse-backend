3674660570e8b52b0ca4c91dfa26102359c766fc
lauk20 <klau20@stuy.edu>
2022-07-09 12:23:40 -0400

    Add user MongoDB model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  courses: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Course"
+  }],
+  assignments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Assignment"
+  }]
+})
+
+userSchema.set("toJSON". {
+  transform: (document, obj) => {
+    delete obj.__v
+  }
+})
+
+module.exports = mongoose.model("User", userSchema)
