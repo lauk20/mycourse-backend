1dafc135c2808b1669fbf61ee6f3ed4bdc909bcd
lauk20 <klau20@stuy.edu>
2022-07-09 15:36:34 -0400

    Update app to use users and login router

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ mongoose.connect(url)
 
 const courseRouter = require("./controllers/courses")
 const assignmentRouter = require("./controllers/assignments")
+const userRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
 
 app.use(cors())
 app.use(express.json())
 app.use("/api/courses", courseRouter)
 app.use("/api/assignments", assignmentRouter)
+app.use("/api/users", userRouter)
+app.use("/api/login", loginRouter)
 
 app.listen(3001, () => {
   console.log("Server running on port 3001")
